6de357bc4b874a61b44b4e2bd70010838eeb50eb
Aplysiida <61487136+Aplysiida@users.noreply.github.com>
2022-09-29 09:15:53 +1300

    Continued working on PBIL

--- eda_q2.py
+++ eda_q2.py
@@ -35,13 +35,38 @@ def update_prob(p, learning_rate, individuals):
         p = p + learning_rate * (ind - p)
     return p
 
-def PBIL(fitness_func, feature_num = 5, pop_size = 50, max_iter = 100, num_best = 5, num_worst = 5, max_p = 0.9, min_p = 0.1, learning_rate=0.1):
-    p = np.linspace(start=0.5, stop=0.5, num=feature_num) #initialise prob vector
-    #pop = [gen_individual(p, feature_num) for _ in range(pop_size)] 
+def mutate_prob(p, mut_rate, mut_shift):
+    return np.array([
+        p_i * (1.0-mut_shift) + rng.random() * mut_shift if(rng.random() < mut_rate) else p_i
+        for p_i in p
+        ])
 
-    #for i in range(max_iter):  #while not stop, todo: add convergence
+"""
+    fitness_func = function to use to measure individual's fitness
+    feature_num = length of individual
+    pop_size = number of individuals in one population
+    max_iter = stopping criteria
+    mut_rate = probability of mutation occuring (0,1)
+    mut_shift = amount of mutation to affect the prob vector
+    num_best = number of best individuals
+    num_worst =  number of worst individuals
+    max_p = maximum value allowed in prob vector
+    min_p = minimum value allowed in prob vector
+    learning_rate = used for prob vector
+"""
+
+def PBIL(
+    fitness_func, feature_num = 5, pop_size = 5, max_iter = 100, 
+    mut_rate = 0.1, mut_shift = 0.1, num_best = 5, num_worst = 5, max_p = 0.9, min_p = 0.1, learning_rate=1.0):
+    p = np.linspace(start=0.5, stop=0.5, num=feature_num) #initialise prob vector
+
+    print(p)
+
+    #for i in range(max_iter):  #while not reached stopping criteria
     pop = [gen_individual(p, feature_num) for _ in range(pop_size)] #use p vector to gen pop
     pop.sort(key=fitness_func, reverse=True)  #sort individuals by fitness
+
+    [print(i) for i in pop]
 
     best_individuals = pop[:num_best]
     worst_individuals = pop[pop_size - num_worst:]
@@ -49,11 +74,10 @@ def PBIL(fitness_func, feature_num = 5, pop_size = 50, max_iter = 100, num_best 
     p = update_prob(p, learning_rate, best_individuals)   #for best individuals
     p = update_prob(p, (-1.0 * learning_rate), worst_individuals)   #for worst individuals
 
-    #mutate p
+    p = mutate_prob(p, mut_rate=mut_rate, mut_shift=mut_shift)   #mutate p
     p = np.array([np.maximum(np.minimum(p_i, max_p),min_p) for p_i in p])   #clamp p
     
     #return best individual
-
 
 if __name__ == "__main__":
     filepaths = sys.argv[1:]
