03127631257db0cac2c2216089dc20d03c3e7256
VicsStuff <61487136+Aplysiida@users.noreply.github.com>
2022-10-02 23:37:12 +1300

    Started ep

--- ep_de_es_q3.py
+++ ep_de_es_q3.py
@@ -13,8 +13,19 @@ def griewanks(points):
     product = 1.0
     for i,x in enumerate(points):
         sum += (pow(x,2))/4000.0
-        product *= np.cos(x/(np.sqrt(i+1)))
+        product *= np.cos(x/(np.sqrt(i+1))) #i+1 since python starts at 0
     return sum - product + 1.0
+
+def EP(pop_size=50, max_iter=1000):
+    #gen pop
+    #gen variance
+    #while not stopping
+        #calc fitness for each individual
+        #for each individual in pop
+            #do something
+        #get best individuals from both parent pop and mutated pop and their variences to get next generation's parent pop
+    #return best
+    print('hello')
 
 if __name__ == "__main__":
     #generate data 
